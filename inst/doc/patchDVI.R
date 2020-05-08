1d3ae3285d2b4ca7e1c348603c1b8caf6804fbe8
Duncan Murdoch <murdoch.duncan@gmail.com>
2020-05-08 08:30:06 +0000

    version 1.10.1

--- inst/doc/patchDVI.R
+++ inst/doc/patchDVI.R
@@ -1,5 +1,4 @@
 ### R code from vignette source 'patchDVI.Rnw'
-### Encoding: UTF-8
 
 ###################################################
 ### code chunk number 1: patchDVI.Rnw:84-85 (eval = FALSE)
@@ -14,9 +13,35 @@
 
 
 ###################################################
-### code chunk number 3: patchDVI.Rnw:101-103 (eval = FALSE)
+### code chunk number 3: patchDVI.Rnw:95-96 (eval = FALSE)
+###################################################
+## opts_knit$set(concordance = TRUE)
+
+
+###################################################
+### code chunk number 4: patchDVI.Rnw:106-108 (eval = FALSE)
+###################################################
+## library(patchDVI)
+## knitPDF("doc.Rnw")
+
+
+###################################################
+### code chunk number 5: patchDVI.Rnw:111-113 (eval = FALSE)
 ###################################################
 ## library(patchDVI)
 ## SweavePDF("doc.Rnw")
+
+
+###################################################
+### code chunk number 6: patchDVI.Rnw:463-464
+###################################################
+patchDVI::needsPackages("patchDVI")
+
+
+###################################################
+### code chunk number 7: patchDVI.Rnw:689-691 (eval = FALSE)
+###################################################
+## .SweaveFiles <- c("a.Rnw", "b.Rnw")
+## patchDVI::knitInRStudio()
 
 
